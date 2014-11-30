77c0ff62c9adb88eb6112cb8085ab41d839018c7
Charles Guo <shaldengeki@gmail.com>
2014-11-30 15:56:26 -0600

    spacing in http_response_read

--- update_daemon/update_daemon.py
+++ update_daemon/update_daemon.py
@@ -25,12 +25,12 @@ import update_modules
 
 # causes httplib to return the partial response from a server in case the read fails to be complete.
 def patch_http_response_read(func):
-    def inner(*args):
-        try:
-            return func(*args)
-        except httplib.IncompleteRead, e:
-            return e.partial
-    return inner
+  def inner(*args):
+    try:
+      return func(*args)
+    except httplib.IncompleteRead, e:
+      return e.partial
+  return inner
 httplib.HTTPResponse.read = patch_http_response_read(httplib.HTTPResponse.read)
 
 class Daemon(object):
